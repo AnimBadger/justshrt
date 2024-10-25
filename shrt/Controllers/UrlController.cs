31501202a978c59f707a6b55821da4c4295b172b
animbadger <anim.ansah.stephen@gmail.com>
2024-10-25 18:19:22 +0000

    first controller

---
+++ shrt/Controllers/UrlController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using shrt.Dtos.requests;
+using shrt.Dtos.response;
+using shrt.models;
+using shrt.services;
+
+namespace shrt.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class UrlController : ControllerBase
+{
+    private readonly UrlService _urlService;
+
+    public UrlController(UrlService urlService)
+    {
+        _urlService = urlService;
+    }
+
+    [HttpPost]
+    public async Task<UrlResponse> ShortenUrl(CreateUrlRequest url)
+    {
+        return await _urlService.CreateShortUrlAsyn(url);
+    }
+    
+}
+
